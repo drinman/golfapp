a515cf8af60e5865082e72132f80754796046399
Dallas Inman <drinman2004@gmail.com>
2024-10-23 13:49:27 -0700

    Initial Commit

---
+++ GolfApp/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  GolfApp
+//
+//  Created by Dallas.Inman on 10/23/24.
+//
+// Dallas
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GolfApp/GolfAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GolfAppApp.swift
+//  GolfApp
+//
+//  Created by Dallas.Inman on 10/23/24.
+// Dallas
+
+import SwiftUI
+
+@main
+struct GolfAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
